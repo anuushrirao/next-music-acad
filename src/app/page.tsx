a11a41fb0f0b957aa69728738f9ebcc0ca6caad3
anuushrirao <anushkavshrirao@gmail.com>
2025-11-12 11:03:06 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import HeroSection from "@/components/HeroSection";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className=" min-h-screen bg-black/[0.96] antialiased bg-grid-white">
+      <HeroSection/>
+    </div>
+  );
+}
