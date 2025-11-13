a74a5a416bea101b6aafabd77700c730902b24a6
anuushrirao <anushkavshrirao@gmail.com>
2025-11-13 14:25:28 +0530

    added new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import TestimonialCard from "@/components/TestimonialCard";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div className=" min-h-screen bg-black/[0.96] antialiased bg-grid-white">
       <HeroSection/>
+      <FeaturedCourses/>
+      <WhyChooseUs/>
+      <TestimonialCard/>
     </div>
   );
 }
